3664e7cf9442abbf2641136c03691dd6fea04be3
Kaydon Duvall <duvall.kaydon@gmail.com>
2019-10-03 14:34:05 -0600

    starting shopping list

---
+++ Module1/moms-shopping-list-start/index.js
@@ -0,0 +1,8 @@
+
+function addItem(){
+    const list = document.getElementById("list")
+    let input = document.getElementById("title");
+    let newItem = document.createElement("li")
+
+    newItem.innerHTML = "<div>" input.value "</div> 
+}
