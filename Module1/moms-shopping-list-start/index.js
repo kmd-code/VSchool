c5d8991f1aea9cbcc76f7f09968c4376540b2d55
Kaydon Duvall <duvall.kaydon@gmail.com>
2019-10-04 18:22:42 -0600

    finished moms shopping list

--- Module1/moms-shopping-list-start/index.js
+++ Module1/moms-shopping-list-start/index.js
@@ -1,8 +1,26 @@
 
-function addItem(){
+const inputBtn = document.getElementById("addItem");
+const deleteBtn = document.getElementsByClassName("delete");
+    
+inputBtn.addEventListener("click",function(){
     const list = document.getElementById("list")
-    let input = document.getElementById("title");
+    let input = document.getElementById("input");
     let newItem = document.createElement("li")
 
-    newItem.innerHTML = "<div>" input.value "</div> 
+    if (input.value != ''){
+    newItem.innerHTML = "<div>" + input.value + "</div><button class='delete'>X</button>"
+    list.appendChild(newItem);
+    input.value = "";
+    deleteRow();
+    }
+});
+
+deleteRow();
+
+function deleteRow (){
+    for (let i = 0; i < deleteBtn.length; i++){
+        deleteBtn[i].addEventListener("click", function(){
+            deleteBtn[i].parentElement.style.display = "none"
+        });
+    }
 }
