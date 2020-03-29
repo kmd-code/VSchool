2eb0e60701403a49a2ac3c1a1bf745ad6fb9d888
Kaydon Duvall <duvall.kaydon@gmail.com>
2020-03-28 22:05:09 -0600

    some progress on authentication

--- Module6/authentication/routes/todoRouter.js
+++ Module6/authentication/routes/todoRouter.js
@@ -13,6 +13,17 @@ todoRouter.get("/", (req, res, next) => {
   })
 })
 
+//Get todos by user
+todoRouter.get('/user', (req, res, next) => {
+  Todo.find({user: req.user._id}, (err, todos) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(todos)
+  })
+})
+
 // Add new Todo
 todoRouter.post("/", (req, res, next) => {
   req.body.user = req.user._id
@@ -29,7 +40,7 @@ todoRouter.post("/", (req, res, next) => {
 // Delete Todo
 todoRouter.delete("/:todoId", (req, res, next) => {
   Todo.findOneAndDelete(
-    { _id: req.params.todoId },
+    { _id: req.params.todoId, user: req.user._id },
     (err, deletedTodo) => {
       if(err){
         res.status(500)
@@ -43,7 +54,7 @@ todoRouter.delete("/:todoId", (req, res, next) => {
 // Update Todo
 todoRouter.put("/:todoId", (req, res, next) => {
   Todo.findOneAndUpdate(
-    { _id: req.params.todoId },
+    { _id: req.params.todoId, user: req.user._id },
     req.body,
     { new: true },
     (err, updatedTodo) => {
